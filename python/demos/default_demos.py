fa65328370e02a00ec28718d97f96623913aec0a
fablechris <chris.wheeler@fable-studio@gmail.com>
2024-04-19 19:52:30 -0700

    SIM-2909: Add new skill classes to formalize skill creation. Rename interactable 'item_guid' to just 'guid' for consistency.

--- python/demos/default_demos.py
+++ python/demos/default_demos.py
@@ -3,6 +3,7 @@ import datetime
 
 from fable_saga.actions import Action
 
+from thistle_gulch.data_models import ReflectSkill, WaitSkill
 from . import Demo, RuntimeBridge, formatted_input
 
 
@@ -102,14 +103,11 @@ class DefaultTutorial(Demo):
                 future = asyncio.get_event_loop().create_future()
                 await bridge.runtime.api.override_character_action(
                     "jack_kane",
-                    Action(
-                        "reflect",
-                        {
-                            "focus": "The current situation in Thistle Gulch.",
-                            "result": "Bring the audience up to speed on who blackjack is and the context of the story.",
-                            "goal": "Bring the audience up to speed on who blackjack is and the context of the story.",
-                        },
-                    ),
+                    ReflectSkill(
+                        focus="The current situation in Thistle Gulch.",
+                        result="Bring the audience up to speed on who blackjack is and the context of the story.",
+                        goal="Bring the audience up to speed on who blackjack is and the context of the story.",
+                    ).to_action(),
                     future=future,
                 )
                 await future
@@ -170,7 +168,9 @@ class DefaultTutorial(Demo):
                 future = asyncio.get_event_loop().create_future()
                 await bridge.runtime.api.override_character_action(
                     "jack_kane",
-                    Action("wait", {"duration": 1}),
+                    WaitSkill(
+                        duration=1, goal="Trigger the next action generation"
+                    ).to_action(),
                     future=future,
                 )
                 await future
